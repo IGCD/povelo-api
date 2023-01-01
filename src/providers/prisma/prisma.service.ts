174d931c8923e1086253ade5ece5d81d30f1fcf1
k3srpn <k3srpn@gmail.com>
2023-01-01 22:31:58 +0900

    refator(prisma): onModuleDestroy -> enableShutdownHooks

--- src/providers/prisma/prisma.service.ts
+++ src/providers/prisma/prisma.service.ts
@@ -1,13 +1,23 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
         await this.$connect();
     }
 
-    async onModuleDestroy() {
-        await this.$disconnect();
+    /**
+     *
+     * We don't bother with onModuleDestroy, since Prisma has its own shutdown hooks where it will destroy the connection
+     * Prisma interferes with NestJS enableShutdownHooks.
+     * Prisma listens for shutdown signals and will call process.exit() before your application shutdown hooks fire.
+     * To deal with this, you would need to add a listener for Prisma beforeExit event.
+     * @docs : https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
+     */
+    async enableShutdownHooks(app: INestApplication) {
+        this.$on('beforeExit', async () => {
+            await app.close();
+        });
     }
 }
