7d6d42452b3e35a359d91e531f786a27a0874389
k3srpn <k3srpn@gmail.com>
2023-01-01 22:19:00 +0900

    feat(login): basic login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
+    constructor(private authService: AuthService) {}
+
     @Post('login')
-    async login(@Body() body: LoginDto) {}
+    async login(@Body() data: LoginDto) {
+        return await this.authService.login(data.email, data.password);
+    }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PASSWORD_VALIDATION_FAILED, USER_NOT_FOUND } from 'src/errors/errors.constant';
+import { PrismaService } from 'src/providers/prisma/prisma.service';
+import crypto from 'crypto';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService, private configService: ConfigService) {}
+
+    async login(email: string, password: string) {
+        //* Validate User
+        await this.userValidator(email, password);
+
+        return true;
+    }
+
+    async userValidator(email: string, password: string) {
+        //* Check is Valid User Email
+        const user = await this.prisma.user
+            .findUniqueOrThrow({
+                where: { email },
+            })
+            .catch((error) => {
+                throw new BadRequestException(USER_NOT_FOUND);
+            });
+
+        //* Check Password is correct
+        if (user.password !== this.hasingPassword(password)) throw new BadRequestException(PASSWORD_VALIDATION_FAILED);
+
+        return true;
+    }
+
+    hasingPassword(password: string) {
+        const salt = this.configService.get('salt');
+
+        return crypto.createHmac('sha256', salt).update(password).digest('hex');
+    }
+}
