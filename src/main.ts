4ee073135d8ecb554fcb0bfcdc4ccbb8d2408738
k3srpn <k3srpn@gmail.com>
2023-01-05 00:38:50 +0900

    feat(): install cookie-parser & enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    const app = await NestFactory.create(AppModule, { cors: true });
+
+    app.enableCors({
+        origin: '*',
+        credentials: true,
+    });
+
+    app.use(cookieParser());
     await app.listen(process.env.NODE_PORT ?? 3000);
 }
 bootstrap();
