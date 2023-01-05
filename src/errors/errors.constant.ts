066c6e05df5423976c87ae505489fb4e09de9dbe
k3srpn <k3srpn@gmail.com>
2023-01-06 02:07:40 +0900

    feat(errors): post not found

--- src/errors/errors.constant.ts
+++ src/errors/errors.constant.ts
@@ -1,5 +1,6 @@
 export const ENV_NOT_FOUND = '404001: 환경변수 키를 찾을 수 없습니다';
 export const USER_NOT_FOUND = '404002: 회원을 찾을 수 없습니다';
+export const POST_NOT_FOUND = '404003: 게시물을 찾을 수 없습니다';
 
 export const LOGIN_FAILED = '400001: 로그인에 실패하였습니다';
 export const REGIST_FAILED = '400002: 회원가입에 실패하였습니다';
