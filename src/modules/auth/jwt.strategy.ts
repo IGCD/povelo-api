870e0f56021731fad086d302e5fb07cd67f1783b
k3srpn <k3srpn@gmail.com>
2023-01-05 23:42:57 +0900

    remove(console.log): request cookie log

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -9,12 +9,7 @@ import { JwtPayload } from './jwt-payload.interface';
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(configService: ConfigService) {
         super({
-            jwtFromRequest: ExtractJwt.fromExtractors([
-                (req: Request) => {
-                    console.log(req.cookies);
-                    return req.cookies.accessToken;
-                },
-            ]),
+            jwtFromRequest: ExtractJwt.fromExtractors([(req: Request) => req.cookies.accessToken]),
             ignoreExpiration: false,
             secretOrKey: configService.get<Configuration['jwt']>('jwt')?.accessKey,
         });
