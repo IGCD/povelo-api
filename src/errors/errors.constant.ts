c27dd4ba99113f23fa3ffbbe1a02aa47dc35d557
k3srpn <k3srpn@gmail.com>
2023-01-06 21:30:01 +0900

    rename(errors): rename error constants

--- src/errors/errors.constant.ts
+++ src/errors/errors.constant.ts
@@ -1,7 +1,7 @@
-export const ENV_NOT_FOUND = '404001: 환경변수 키를 찾을 수 없습니다';
-export const USER_NOT_FOUND = '404002: 회원을 찾을 수 없습니다';
-export const POST_NOT_FOUND = '404003: 게시물을 찾을 수 없습니다';
+export const NOT_FOUND_ENV = '404001: 환경변수 키를 찾을 수 없습니다';
+export const NOT_FOUND_USER = '404002: 회원을 찾을 수 없습니다';
+export const NOT_FOUND_POST = '404003: 게시물을 찾을 수 없습니다';
 
-export const LOGIN_FAILED = '400001: 로그인에 실패하였습니다';
-export const REGIST_FAILED = '400002: 회원가입에 실패하였습니다';
-export const TOKEN_FAILED = '400003: 올바르지 않은 접근입니다';
+export const BAD_REQUEST_LOGIN = '400001: 로그인에 실패하였습니다';
+export const BAD_REQEUST_REGIST = '400002: 회원가입에 실패하였습니다';
+export const UNAUTHORIZED_TOKEN = '400003: 올바르지 않은 접근입니다';
