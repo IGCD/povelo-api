eb23dc3e57d55f6a9b1939a929477fea8bcaec62
k3srpn <k3srpn@gmail.com>
2023-01-07 23:54:59 +0900

    rename(error): rename error constants

--- src/errors/errors.constant.ts
+++ src/errors/errors.constant.ts
@@ -1,9 +1,8 @@
-export const NOT_FOUND_ENV = '404001: Not found environment Key';
-export const NOT_FOUND_USER = '404002: Not found user';
-export const NOT_FOUND_POST = '404003: Not found post';
-
-export const BAD_REQUEST_LOGIN = '400001: Failed to login';
-export const BAD_REQEUST_REGIST = '400002: Failed to regist';
+export const ENV_NOT_FOUND = '404001: Environment Key not found';
+export const USER_NOT_FOUND = '404002: User not found';
+export const POST_NOT_FOUND = '404003: Post not found';
+export const SESSION_NOT_FOUND = '404004: Session not found';
+export const INVALID_LOGIN = '400001: Invalid email or password entered';
 export const INVALID_TOKEN = '400003: Invalid token';
-export const EXIST_EMAIL = '400004: Email already exist';
-export const EXIST_PHONENUMBER = '400005: Phone number already exist';
+export const EMAIL_USER_CONFLICT = '409004: User with this email already exists';
+export const PHONE_USER_CONFLICT = '409005: User with this phone number already exists';
