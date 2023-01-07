aa27cf8869b4fdd6ad600a824a632bcc89cbc616
k3srpn <k3srpn@gmail.com>
2023-01-07 23:53:41 +0900

    remove(jwt-refresh): remove refresh starategy for automation

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -4,18 +4,26 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Configuration } from 'src/config/configuration.interface';
-import { JwtPayload } from './jwt-payload.interface';
+import { TokenService } from 'src/providers/token/token.service';
+import { JwtPayload } from './auth.interface';
+import { AuthService } from './auth.service';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(configService: ConfigService) {
+    constructor(configService: ConfigService, private tokenService: TokenService, private authService: AuthService) {
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([(req: Request) => req.cookies.accessToken]),
             ignoreExpiration: false,
-            secretOrKey: configService.get<Configuration['jwt']>('jwt')?.accessKey,
+            secretOrKey: configService.get<Configuration['security']['jwt']['accessSecret']>('security.jwt.accessSecret'),
+            passReqToCallback: true,
         });
     }
 
-    async validate(payload: JwtPayload): Promise<any> {
-        return { id: payload.id };
+    async validate(request: Request): Promise<any> {
+        const token = request.cookies['accessToken'];
+        try {
+            return this.tokenService.verify(token);
+        } catch (error) {
+            await this.authService.refresh(token);
+        }
     }
 }
