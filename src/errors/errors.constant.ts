45aef3464198c54ce6baa25eeb51436313865501
k3srpn <k3srpn@gmail.com>
2023-01-07 23:52:55 +0900

    rename(errors):rename error constants

--- src/errors/errors.constant.ts
+++ src/errors/errors.constant.ts
@@ -1,7 +1,9 @@
-export const NOT_FOUND_ENV = '404001: 환경변수 키를 찾을 수 없습니다';
-export const NOT_FOUND_USER = '404002: 회원을 찾을 수 없습니다';
-export const NOT_FOUND_POST = '404003: 게시물을 찾을 수 없습니다';
+export const NOT_FOUND_ENV = '404001: Not found environment Key';
+export const NOT_FOUND_USER = '404002: Not found user';
+export const NOT_FOUND_POST = '404003: Not found post';
 
-export const BAD_REQUEST_LOGIN = '400001: 로그인에 실패하였습니다';
-export const BAD_REQEUST_REGIST = '400002: 회원가입에 실패하였습니다';
-export const UNAUTHORIZED_TOKEN = '400003: 올바르지 않은 접근입니다';
+export const BAD_REQUEST_LOGIN = '400001: Failed to login';
+export const BAD_REQEUST_REGIST = '400002: Failed to regist';
+export const INVALID_TOKEN = '400003: Invalid token';
+export const EXIST_EMAIL = '400004: Email already exist';
+export const EXIST_PHONENUMBER = '400005: Phone number already exist';
